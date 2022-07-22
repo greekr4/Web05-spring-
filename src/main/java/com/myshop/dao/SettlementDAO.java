8ff03f914f192ba71a5eb8a4c9ac9c5ee6dae884
greekr4 <greekr4@naver.com>
2022-07-22 22:05:42 +0900

    0722

--- src/main/java/com/myshop/dao/SettlementDAO.java
+++ src/main/java/com/myshop/dao/SettlementDAO.java
@@ -2,9 +2,14 @@ package com.myshop.dao;
 
 import java.util.List;
 
-import com.myshop.dto.RecentlyDTO;
+import com.myshop.dto.SettlementDTO;
 
 public interface SettlementDAO {
 	//정산 리스트
+	public List<SettlementDTO> SettlementList() throws Exception;
+	//정산 추가
+	public void SettlementAdd(SettlementDTO DTO) throws Exception;
+	//정산 삭제
+	public void SettlementDel(int seq) throws Exception;
 	
 }
